9084531f6cd86c636722dffbdcfe1f5b60d9af96
adityastar432@gmail.com <adityastar432@gmail.com>
2025-01-04 13:46:15 +0530

    resolve conflict

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,12 +9,7 @@ import { LoginMainComponent } from './auth/login-main/login-main.component';
 import { ForgotPasswordPopupComponent } from './auth/forgot-password-popup/forgot-password-popup.component';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { CourseEnrollmentComponent } from './modules/student/components/course-enrollment/course-enrollment.component'; // Import the component
-import { MyLearningComponent } from './modules/student/components/my-learning/my-learning.component';
-import { StudentsAllCoursesComponent } from './modules/student/components/my-learning/my-learning-sub-comonents/students-all-courses/students-all-courses.component';
-import { StudentsMyListsComponent } from './modules/student/components/my-learning/my-learning-sub-comonents/students-my-lists/students-my-lists.component';
-import { StudentsWishlistComponent } from './modules/student/components/my-learning/my-learning-sub-comonents/students-wishlist/students-wishlist.component';
-import { StudentsArchivedComponent } from './modules/student/components/my-learning/my-learning-sub-comonents/students-archived/students-archived.component';
-import { StudentsLearningToolsComponent } from './modules/student/components/my-learning/my-learning-sub-comonents/students-learning-tools/students-learning-tools.component';
+
 
 export const routes: Routes = [
   { path: 'cyber', component: FooterSectionComponent },
@@ -30,20 +25,7 @@ export const routes: Routes = [
     children: [
       { path: 'dashboard', component: DashboardComponent },
       { path: 'navbar', component: NavbarComponent },
-      { path: 'courses', component: CourseEnrollmentComponent },
-      { 
-        path: 'my-learning',
-        component: MyLearningComponent,
-        canActivate: [AuthGuard],
-        children: [
-          { path: '', component: StudentsAllCoursesComponent},
-          { path: "my-lists", component: StudentsMyListsComponent},
-          { path: "wishlist", component: StudentsWishlistComponent},
-          { path: 'archived', component: StudentsArchivedComponent},
-          { path: 'learning-tools', component: StudentsLearningToolsComponent}
-        ]
-
-      }
+      { path: 'courses', component: CourseEnrollmentComponent }
     ]
   },
   
