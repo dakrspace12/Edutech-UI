fa471f8355deb55ba71b282db6d2b9c586c6613e
Sarvesh Adgaonkar <sarveshadgaonkar5@gmail.com>
2025-01-06 12:58:34 +0530

    admin dashboard ui

--- src/app/modules/admin/admin-dashboard/admin-dashboard.component.ts
+++ src/app/modules/admin/admin-dashboard/admin-dashboard.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-admin-dashboard',
-  standalone: true,
-  imports: [],
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.scss'
+  styleUrls: ['./admin-dashboard.component.scss']
 })
-export class AdminDashboardComponent {
+export class AdminDashboardComponent implements OnInit {
+  totalCourses: number;
+  totalUsers: number;
+  monthlyRevenue: number;
+  recentActivities: string[];
 
+  constructor() {
+  
+    this.totalCourses = 120;
+    this.totalUsers = 4500;
+    this.monthlyRevenue = 12000;
+    this.recentActivities = [
+      'User John Doe enrolled in "Angular Basics"',
+      'New course "Advanced TypeScript" added',
+      'User Jane Smith completed "React for Beginners"'
+    ];
+  }
+
+  ngOnInit(): void {
+    
+  }
 }
