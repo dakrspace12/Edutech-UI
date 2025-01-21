ffafb61c9f0b7af08f26a2fd30b588e0fcf7ca9d
ANAND KUMAR <anand9018@hotmail.com>
2025-01-21 16:48:56 +0530

    added user id and decoded token to get user-id to access id end point

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -13,11 +13,13 @@ export class AuthGuard implements CanActivate {
     if (token) {
       try {
         const decodedToken: any = jwt_decode.jwtDecode(token);
-        if (decodedToken.exp * 1000 < Date.now()) {
+        if (decodedToken.exp && decodedToken.exp * 1000 < Date.now()) {
      
           this.router.navigate(['/login']);
           return false;
         }
+        const userId = decodedToken.id;
+        console.log('User ID:', userId);
       return true;
     } catch (error) {
       console.error('Invalid token', error);
@@ -25,6 +27,7 @@ export class AuthGuard implements CanActivate {
       return false;
     }
   }
+    console.log('No token found');
     this.router.navigate(['/login']);
     return false;
    }
