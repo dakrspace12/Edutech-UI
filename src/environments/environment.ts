4630287448ebaaa7d9535c158c8750b69cc1af9a
adityastar432@gmail.com <adityastar432@gmail.com>
2025-02-06 17:50:07 +0530

    Course Post Method Done

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:8080/auth'
+    apiUrl: 'http://localhost:8080/auth',
+    courseApiUrl: 'http://localhost:8080/courses'
   };
   
